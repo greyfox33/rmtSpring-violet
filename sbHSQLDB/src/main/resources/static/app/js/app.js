832136c27602bc5171ea042c24070200fa6fdefa
Robb Thompson <robb@oakts.com>
2015-06-01 20:28:30 -0700

    angular route working

--- sbHSQLDB/src/main/resources/static/app/js/app.js
+++ sbHSQLDB/src/main/resources/static/app/js/app.js
@@ -2,23 +2,23 @@
 
 /* App Module */
 
-var phonecatApp = angular.module('phonecatApp', [
+var childApp = angular.module('childApp', [
   'ngRoute',
-  'phonecatControllers'
+  'childControllers'
 ]);
 
-phonecatApp.config(['$routeProvider',
+childApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/child', {
         templateUrl: 'partials/child-list.html',
         controller: 'ChildListCtrl'
       }).
-      when('/child/:phoneId', {
+      when('/childdetail/:childId', {
         templateUrl: 'partials/child-detail.html',
         controller: 'ChildDetailCtrl'
       }).
       otherwise({
-        redirectTo: '/child'
+        redirectTo: 'child'
       });
   }]);
--- sbHSQLDB/src/main/resources/static/app/js/controllers.js
+++ sbHSQLDB/src/main/resources/static/app/js/controllers.js
@@ -6,23 +6,21 @@ var childControllers = angular.module('childControllers', []);
 
 childControllers.controller('ChildListCtrl', ['$scope', '$http',
   function($scope, $http) {
-    $http.get('child/').success(function(data) {
+    $http.get('../child/').success(function(data) {
       $scope.children = data;
     });
-
-    $scope.orderProp = 'dob';
   }]);
 
 childControllers.controller('ChildDetailCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
-    $http.get('child/' + $routeParams.childId ).success(function(data) {
-      $scope.child = data;
+    $http.get('../child/' + $routeParams.childId ).success(function(data) {
+      $scope.childDetail = data;
     });
   }]);
 
 childControllers.controller('ChildDetailStatusCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
-    $http.get('status/' + $routeParams.childId ).success(function(data) {
+    $http.get('../status/' + $routeParams.childId ).success(function(data) {
       $scope.childStatus = data;
     });
   }]);
