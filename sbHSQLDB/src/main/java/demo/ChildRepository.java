2bad2ef144608b6e38f54b00b54f7ca22c85ba69
Robb Thompson <robb@oakts.com>
2015-06-03 10:31:41 -0700

    updates for angular access

--- sbHSQLDB/src/main/java/demo/ChildRepository.java
+++ sbHSQLDB/src/main/java/demo/ChildRepository.java
@@ -12,12 +12,6 @@ public interface ChildRepository extends CrudRepository<Child, Long> {
 
     List<Child> findByLast(String last);
     
-//    @Query("select u from User u where u.emailAddress = ?1")
-//    User findByEmailAddress(String emailAddress);
-    
-    // @Query(value = "Select * From Child")
-    //public List<Child> all();
-
 }
 
 
